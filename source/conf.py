8aa070f8826216585bd8fa976deeac981c6d66a3
Dmitry Baryshnikov <bishop.dev@gmail.com>
2016-01-20 13:05:32 +0300

    fix pdf duild

--- source/conf.py
+++ source/conf.py
@@ -120,7 +120,7 @@ todo_include_todos = True
 
 numfig = True
 
-numfig_format = {'figure': u'Рис. %s.', 'table': u'Таблица %s.', 'code-block': u'Листинг %s.'}
+numfig_format = {'figure': u'Рис. %s.', 'table': u'Таблица %s.', 'code-block': u'Listing %s.'}
 
 illufig_format = {'illustrate': u'Рис. %s (%s).'}
 
