14b919659a9ec88220aacf8de8b5aefe4d1cb7b2
Dmitry Baryshnikov <bishop.dev@gmail.com>
2016-04-26 22:02:02 +0300

    Fix edit on github for en translation

--- source/_ext/edit_on_github.py
+++ source/_ext/edit_on_github.py
@@ -46,6 +46,10 @@ def html_page_context(app, pagename, templatename, context, doctree):
         repo = 'nextgisweb'
         path = os.path.join(*dirs[1:])
         branch = '2'
+    elif dirs[0].startswith('qms_srv_dev'):
+        repo = 'quickmapservices_server'
+        path = os.path.join(*dirs[1:])
+        branch = 'master'    
     elif dirs[0].startswith('docs_'):
         repo = dirs[0]  
         path = os.path.join(*dirs[2:])        
