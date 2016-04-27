bd5a2369442676af48a87beea7c1cb4dd16c433a
Dmitry Baryshnikov <bishop.dev@gmail.com>
2016-04-27 11:33:50 +0300

    update submodules

---
+++ switch_lang.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#switch lang - swithc_lang.sh ru
+
+git checkout $1
+cd source/docs_formbuilder
+git checkout $1
+cd ../docs_ngmanager
+git checkout $1
+cd ../docs_ngmobile
+git checkout $1
+cd ../docs_ngqgis
+git checkout $1
+cd ../docs_ngweb
+git checkout $1
+cd ../../
+
+exit 0
