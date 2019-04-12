e88c66685bdef0bc267cd2bef0dc54d2fb5f49cf
Dmitry Baryshnikov <bishop.dev@gmail.com>
2019-04-13 01:53:25 +0300

    Fix switch lang

--- switch_lang.sh
+++ switch_lang.sh
@@ -18,6 +18,28 @@ cd ../docs_simplecollector
 git checkout $1
 cd ../docs_collector
 git checkout $1
+cd ../docs_forestinspector
+git checkout master
+cd ../docs_formbuilder_dev
+git checkout master
+cd ../docs_formbuilder_dev
+git checkout master
+cd ../docs_howto
+git checkout master
+cd ../docs_ngbio
+git checkout master
+cd ../docs_ngcourses
+git checkout master
+cd ../docs_ngweb_dev
+git checkout 3
+cd ../docs_ogportal
+git checkout master
+cd ../ngmobile_dev/ios_maplib_src
+git checkout master
+cd ../../ngmobile_dev/maplib_src
+git checkout master
+cd ../qms_srv_dev
+git checkout master
 cd ../../
 
 exit 0
