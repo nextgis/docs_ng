24b20698ef2f0fe8c2397c73946b537ab6c7246d
Maxim Dubinin <maxim.dubinin@nextgis.com>
2025-03-11 12:09:23 +0300

    try to fix path issue

--- switch_lang.sh
+++ switch_lang.sh
@@ -3,7 +3,11 @@
 
 git checkout $1
 
-REPOS="docs_ngcom docs_toolbox docs_data docs_geoservices docs_ngweb docs_ngqgis docs_ngmobile docs_geoserv_prem docs_toolbox_prem docs_formbuilder docs_collector docs_ngtracker docs_ngid docs_ngconnect docs_rosreestr_tools docs_ngweb_3D ngmobile_dev qms_srv_dev docs_formbuilder_dev tracker_hub_dev docs_howto docs_ngweb_dev docs_ngwdocker"
+REPOS="docs_toolbox docs_data docs_geoservices docs_ngweb docs_ngqgis docs_ngmobile docs_geoserv_prem docs_toolbox_prem docs_formbuilder docs_collector docs_ngtracker docs_ngid docs_ngconnect docs_rosreestr_tools docs_ngweb_3D ngmobile_dev qms_srv_dev docs_formbuilder_dev tracker_hub_dev docs_howto docs_ngweb_dev docs_ngwdocker"
+
+cd source/docs_ngcom
+echo "$(pwd)"
+git checkout $1
 
 for repo in $REPOS; do
     if [ -d "../$repo" ]; then
