a9da3a191ea11c4c5b7d99e436c01431244d95cb
Maxim Dubinin <maxim.dubinin@nextgis.com>
2025-03-11 11:47:23 +0300

    rewrite in a better way

--- switch_lang.sh
+++ switch_lang.sh
@@ -1,134 +1,43 @@
 #!/bin/bash
-#switch lang - swithc_lang.sh ru
+#switch lang - switch_lang.sh en
 
 git checkout $1
 
-cd source/docs_formbuilder
-echo "$(pwd)"
-git checkout $1
+REPOS=(
+    "docs_ngcom"
+    "docs_toolbox"
+    "docs_data"
+    "docs_geoservices"
+    "docs_ngweb"
+    "docs_ngqgis"
+    "docs_ngmobile"
+    "docs_geoserv_prem"
+    "docs_toolbox_prem"
+    "docs_formbuilder"
+    "docs_collector"
+    "docs_ngtracker"
+    "docs_ngid"
+    "docs_ngconnect"
+    "docs_rosreestr_tools"
+    "docs_ngweb_3D"
+    "ngmobile_dev"
+    "qms_srv_dev"
+    "docs_formbuilder_dev"
+    "tracker_hub_dev"
+    "docs_howto"
+    "docs_ngweb_dev"
+    "docs_ngwdocker"
+)
 
-cd ../docs_ngmanager
-echo "$(pwd)"
-git checkout $1
-
-cd ../docs_ngmobile
-echo "$(pwd)"
-git checkout $1
-
-cd ../docs_ngqgis
-echo "$(pwd)"
-git checkout $1
-
-cd ../docs_ngweb
-echo "$(pwd)"
-git checkout $1
-
-cd ../docs_ngcom
-echo "$(pwd)"
-git checkout $1
-
-cd ../docs_data
-echo "$(pwd)"
-git checkout $1
-
-cd ../docs_ngid
-echo "$(pwd)"
-git checkout $1
-
-if [ -d "../docs_geoserv_prem" ]; then
-cd ../docs_geoserv_prem
-echo "$(pwd)"
-git checkout $1
-fi
-
-if [ -d "../docs_collector" ]; then
-cd ../docs_collector
-echo "$(pwd)"
-git checkout $1
-fi
-
-if [ -d "../docs_toolbox" ]; then
-cd ../docs_toolbox
-echo "$(pwd)"
-git checkout $1
-fi
-
-if [ -d "../docs_forestinspector" ]; then
-cd ../docs_forestinspector
-echo "$(pwd)"
-git checkout master
-fi
-
-if [ -d "../docs_formbuilder_dev" ]; then
-cd ../docs_formbuilder_dev
-echo "$(pwd)"
-git checkout master
-fi
-
-if [ -d "../docs_howto" ]; then
-cd ../docs_howto
-echo "$(pwd)"
-git checkout master
-fi
-
-if [ -d "../docs_ngbio" ]; then
-cd ../docs_ngbio
-echo "$(pwd)"
-git checkout master
-fi
-
-if [ -d "../docs_ngcourses" ]; then
-cd ../docs_ngcourses
-echo "$(pwd)"
-git checkout master
-fi
-
-if [ -d "../docs_ngweb_dev" ]; then
-cd ../docs_ngweb_dev
-echo "$(pwd)"
-git checkout master
-fi
-
-if [ -d "../docs_ogportal" ]; then
-cd ../docs_ogportal
-echo "$(pwd)"
-git checkout master
-fi
-
-if [ -d "../ngmobile_dev" ]; then
-  cd ../ngmobile_dev
-  echo "$(pwd)"
-  if [ -d "ios_maplib_src" ]; then
-    cd ios_maplib_src
-    echo "$(pwd)"
-    git checkout master
-    cd ..
-  fi
-  if [ -d "maplib_src" ]; then
-    cd maplib_src
-    echo "$(pwd)"
-    git checkout master
-    cd ..
-  fi
-  if [ -d "maplibui_src" ]; then
-    cd maplibui_src
-    echo "$(pwd)"
-    git checkout master
-    cd ..
-  fi
-fi
-
-if [ -d "../qms_srv_dev" ]; then
-cd ../qms_srv_dev
-echo "$(pwd)"
-git checkout master
-fi
-
-if [ -d "../docs_ngwdocker" ]; then
-cd ../docs_ngwdocker
-echo "$(pwd)"
-git checkout master
-fi
+for repo in "${REPOS[@]}"; do
+    if [ -d "../$repo" ]; then
+        cd "../$repo"
+        echo "Processing $(pwd)..."
+        git checkout $1
+    else
+        echo "Skipping $repo (directory not found)"
+    fi
+done
 
 cd ../../
 echo "$(pwd)"
