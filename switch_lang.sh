7b9c04dcc6e3e2f6c2a66b81275ce83b9a31f8e2
Maxim Dubinin <maxim.dubinin@nextgis.com>
2025-07-25 15:41:08 +0300

    Update switch_lang.sh
    support repos with master as branch

--- switch_lang.sh
+++ switch_lang.sh
@@ -3,7 +3,7 @@
 
 git checkout $1
 
-REPOS="docs_toolbox docs_data docs_geoservices docs_ngweb docs_ngqgis docs_ngmobile docs_geoserv_prem docs_toolbox_prem docs_formbuilder docs_collector docs_ngtracker docs_ngid docs_ngconnect docs_rosreestr_tools docs_ngweb_3D ngmobile_dev qms_srv_dev docs_formbuilder_dev tracker_hub_dev docs_howto docs_ngweb_dev docs_ngwdocker"
+REPOS="docs_toolbox docs_data docs_geoservices docs_ngweb docs_ngqgis docs_ngmobile docs_geoserv_prem docs_toolbox_prem docs_formbuilder docs_collector docs_ngtracker docs_ngid docs_ngconnect docs_rosreestr_tools docs_ngweb_3D ngmobile_dev qms_srv_dev docs_formbuilder_dev tracker_hub_dev docs_howto"
 
 cd source/docs_ngcom
 echo "$(pwd)"
@@ -14,6 +14,18 @@ for repo in $REPOS; do
         cd "../$repo"
         echo "Processing $(pwd)..."
         git checkout $1
+    else
+        echo "Skipping $repo (directory not found)"
+    fi
+done
+
+REPOS_MASTER="docs_ngweb_dev docs_ngwdocker"
+
+for repo in $REPOS_MASTER; do
+    if [ -d "../$repo" ]; then
+        cd "../$repo"
+        echo "Processing $(pwd)..."
+        git checkout master
     else
         echo "Skipping $repo (directory not found)"
     fi
