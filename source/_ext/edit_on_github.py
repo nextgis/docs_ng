56278ef22e7e192c53ca7faf6326a28174573d6c
Maxim Dubinin <maxim.dubinin@nextgis.com>
2026-09-18 14:51:02 +0300

    Refactor edit_on_github.py to simplify repo selection
    Removed specific conditions for documentation repositories.

--- source/_ext/edit_on_github.py
+++ source/_ext/edit_on_github.py
@@ -52,10 +52,6 @@ def html_page_context(app, pagename, templatename, context, doctree):
         repo = 'quickmapservices_server'
         path = os.path.join(*dirs[1:])
         branch = 'master'
-    elif dirs[0].startswith('docs_ogportal') or dirs[0].startswith('docs_ngbio') or dirs[0].startswith('docs_forestinspector') or dirs[0].startswith('docs_ngcourses'):
-        repo = dirs[0]
-        path = os.path.join(*dirs[1:])
-        branch = 'master'
     else:
         repo = dirs[0]
         path = os.path.join(*dirs[1:])
